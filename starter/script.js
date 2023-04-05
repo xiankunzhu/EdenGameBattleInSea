c19d6a1bbec7df1b3ca1b4ad5dfa144b4c58234f
xiankun.zhu <xiankun.zhu@gmail.com>
2023-04-04 21:34:20 -0600

    4/4 add a player

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,87 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 2000;
+    canvas.width = 500;
     canvas.height = 500;
+
+    //classes
+
+    class InputHandler {
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if (((e.key === 'ArrowUp') ||
+                    (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1) {
+                    this.game.keys.push(e.key);
+                }
+            });
+            window.addEventListener('keyup', e => {
+                if (this.game.keys.indexOf(e.key) > -1) {
+                    this.game.keys.splice(this.game.keys.indexOf(e.key, 1), 1)
+                }
+            });
+        }
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedy = 0;
+            this.maxSpeed = 2;
+        }
+        update() {
+            if (this.game.keys.includes('ArrowUp')) this.speedy = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedy = this.maxSpeed;
+            else this.speedy = 0;
+            this.y += this.speedy;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    class Enemy {
+
+    }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
+        }
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
+    }
+    const game = new Game(canvas.width, canvas.height);
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
